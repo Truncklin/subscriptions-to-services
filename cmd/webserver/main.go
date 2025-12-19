8a076a0b36ee84035029774fdc6d2eb9f4584b24
Samurosa <75kno15co10s@gmail.com>
2025-12-20 02:27:05 +0300

    create config env

--- cmd/webserver/main.go
+++ cmd/webserver/main.go
@@ -1,5 +1,40 @@
 package main
 
-func main() {
+import (
+	"log/slog"
+	"os"
 
+	"github.com/spf13/pflag"
+
+	"SubServices/internal/config"
+)
+
+func main() {
+	slogInit()
+
+	configPath := pafseFlags()
+
+	cfg, err := config.MustLoadConfig(configPath)
+	if err != nil {
+		slog.Error("Error loading config", slog.Any("error", err))
+	}
+
+	slog.Info("Config loaded successfully", slog.Any("config", cfg))
+}
+
+func slogInit() {
+	logger := slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{
+		Level: slog.LevelDebug,
+	}))
+	slog.SetDefault(logger)
+}
+
+func pafseFlags() string {
+
+	var configPath string
+
+	pflag.StringVar(&configPath, "config", "configs/local.yaml", "Path to config file")
+	pflag.Parse()
+
+	return configPath
 }
